bed13367f525ce3a94230b78b034bd0d245ae986
WithPau <paudomec@alumnes.ilerna.com>
2025-02-14 16:31:33 +0100

    PUSH
    jcjcj

--- src/app/components/producto/producto.component.ts
+++ src/app/components/producto/producto.component.ts
@@ -1,18 +1,33 @@
-import { Component, input } from '@angular/core';
+import { Component, input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Categoria } from '../../interfaces/categoria.interface';
+import { CommonModule } from '@angular/common';
+import { ThemeService } from '../../services/theme.service';
 import { Producto } from '../../interfaces/producto.interface';
 
 @Component({
   selector: 'app-producto',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './producto.component.html',
   styleUrls: ['./producto.component.css']
 })
-export class ProductoComponent {
+export class ProductoComponent implements OnInit {
   producto = input<Producto>({
     imagen: 'assets/default.png',
     titulo: 'Producto sin título',
     descuento: '0%'
   });
+
+  isDarkMode = false;
+
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit() {
+    this.themeService.darkMode$.subscribe(
+      isDark => this.isDarkMode = isDark
+    );
+  }
 
   get productoValue(): Producto {
     return this.producto();
