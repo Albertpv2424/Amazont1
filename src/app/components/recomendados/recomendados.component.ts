6a2356b01154ad32e197ac1e4d5e1e3f2997ab03
WithPau <paudomec@alumnes.ilerna.com>
2025-03-12 18:47:12 +0100

    push

--- src/app/components/recomendados/recomendados.component.ts
+++ src/app/components/recomendados/recomendados.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ThemeService } from '../../services/theme.service';
 
 @Component({
   selector: 'app-recomendados',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './recomendados.component.html',
   styleUrls: ['./recomendados.component.css']
 })
-export class RecomendadosComponent {
+export class RecomendadosComponent implements OnInit {
+  isDarkMode = false;
+  
   recomendados = [
     { imagen: 'assets/maquillaje.png', descripcion: 'Belleza y Regalos' },
     { imagen: 'assets/regalos.png', descripcion: 'Regalos perfectos para tus seres queridos' }
   ];
+
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit() {
+    this.themeService.darkMode$.subscribe(
+      isDark => this.isDarkMode = isDark
+    );
+  }
 }
