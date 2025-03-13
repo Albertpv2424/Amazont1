40922c98029f9405dc8b4cf750c652b19f720ff6
WithPau <paudomec@alumnes.ilerna.com>
2025-03-13 15:45:00 +0100

    poush

--- src/app/components/categorias/categorias.component.ts
+++ src/app/components/categorias/categorias.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { Categoria } from '../../interfaces/categoria.interface';
-import { CategoriaComponent } from '../categoria/categoria.component';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CategoriaComponent } from '../categoria/categoria.component';
+import { Categoria } from '../../interfaces/categoria.interface';
+import { ThemeService } from '../../services/theme.service';
 
 @Component({
   selector: 'app-categorias',
@@ -10,10 +11,20 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./categorias.component.css'],
   imports: [CategoriaComponent, CommonModule]
 })
-export class CategoriasComponent {
+export class CategoriasComponent implements OnInit {
+  isDarkMode = false;
+  
   categorias: Categoria[] = [
     { nombre: 'Tecnología', imagen: 'assets/tecnologia.png' },
     { nombre: 'Deportes', imagen: 'assets/deportes.png' },
     { nombre: 'Cocina', imagen: 'assets/cocina.png' }
   ];
+  
+  constructor(private themeService: ThemeService) {}
+  
+  ngOnInit() {
+    this.themeService.darkMode$.subscribe(
+      isDark => this.isDarkMode = isDark
+    );
+  }
 }
